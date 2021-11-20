bc1b0ed76d424f156a87c9d5fffb1e07342059f9
Alvaro Armijos <alvaro.armijoss@gmail.com>
2021-11-20 10:15:05 -0500

    google singin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 
@@ -61,6 +62,62 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+
+        const { nombre, img, correo } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //Hay que crear el usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if (!usuario.estado) {
+            return res.status(401).json({
+                en: -1,
+                m: 'Usuario bloqueado'
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            en: 1,
+            m: "login correcto",
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            en: -1,
+            m: 'El token no se pudo verificar'
+        });
+
+    }
+
+
+
+}
+
 module.exports = {
     login,
+    googleSignIn
 }
